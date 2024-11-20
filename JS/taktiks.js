f3d0256027ec025d373b04fa5977fc7a49c93b46
Bekir <aqwwwe73@gmail.com>
2024-11-20 22:10:39 +0300

    taktiks added

---
+++ JS/taktiks.js
@@ -0,0 +1,232 @@
+const images = [
+  "../Datas/Images/3-4-3_SAHA.png", "../Datas/Images/4-2-2-2(DOS-OOS-POKY)_SAHA.png", "../Datas/Images/4-2-3-1(DOS-OOS-WIDE)_SAHA.png",
+  "../Datas/Images/4-2-4(DOS-WIDE)_SAHA.png", "../Datas/Images/4-3-2-1(DOS-COS-POKY)_SAHA.png", "../Datas/Images/4-3-3(DOS-WIDE)_SAHA.png",
+  "../Datas/Images/4-4-2_SAHA.png", "../Datas/Images/5-2-1-2(DOS-OOS)_SAHA.png", "../Datas/Images/5-2-2-1(DOS-OOS)_SAHA.png",
+  "../Datas/Images/5-2-2(DOS-KB)_SAHA.png", "../Datas/Images/5-2-3(DOS-WIDE)_SAHA.png", "../Datas/Images/4-4-2(DIAMOND-POKY)_SAHA.png"
+];
+
+const tactics = [
+  {
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },
+  {
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },{
+    poses: [
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+      new Vector2(0, 0),
+    ],
+    name: " TACTIC'S NAME ",
+    secondName: " ", 
+  },
+];
+
+let created = false;
+
+
+function CreateTaktiks(){
+  if(created) return;
+  
+  const parentDiv = document.querySelector(".openedLabel");
+  tactics.forEach((_, index)=>{
+    const newDiv = document.createElement("div");
+    const newImg = document.createElement("img");
+    const newTransparentDiv = document.createElement("div");
+
+    newDiv.classList.add("tactic-item");
+    newDiv.id = `tactic-item-${index}`;
+
+    newTransparentDiv.classList.add("tactic-item-Tranparancy");
+
+    newImg.src = images[index]; 
+    newDiv.appendChild(newTransparentDiv);
+    newDiv.appendChild(newImg);
+    parentDiv.appendChild(newDiv);
+  });
+  created = true;
+}
+
+const onMouseEnter = () =>{
+  
+}
