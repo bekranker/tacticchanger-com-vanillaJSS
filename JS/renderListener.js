748c3b4f1f00ea003680191a76e3a8435a622484
Bekir <aqwwwe73@gmail.com>
2024-11-27 22:33:46 +0300

    srry for comments my cat just wrote them

--- JS/renderListener.js
+++ JS/renderListener.js
@@ -10,11 +10,14 @@ const frameProp = {
   startTime: Date.now(),
   currentTime: 0,
 }
+
+//guess what this variable does o_o
 let deltaTime = 0;
 
 //our own mthrfckr deltaTime and Update methods. Who do fuck need react ?
 function OnTick(){
   frameProp.currentTime = Date.now();
+  //this is the differences between your mother's dick and my dic's size. srry
   deltaTime = (frameProp.currentTime - frameProp.startTime) / 1000;
   console.log(deltaTime);
 }
