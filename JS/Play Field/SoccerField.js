d48886d7936ca9f8492bb52b62a0dfb6e3b06d73
Bekir <aqwwwe73@gmail.com>
2024-11-28 23:45:01 +0300

    Background functionalty added

--- JS/Play Field/SoccerField.js
+++ JS/Play Field/SoccerField.js
@@ -1,11 +1,11 @@
 const BG_GREEN = '../../Images/Background_Green.png';
 
-
+let ctx;
 // This is the main call for all render and create seccions;
 function DrawCanvas(){
   
   const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext('2d');
+  ctx = canvas.getContext('2d');
   
   const canvasParent = document.getElementById("canvas-parent");
   
@@ -14,27 +14,26 @@ function DrawCanvas(){
   canvas.classList.add("canvas");
   canvasParent.appendChild(canvas);
 
-  changeBackground(ctx, BG_GREEN, ()=>{
-    for (let i = 1; i <= 12; i++) {
-      playerComponent(ctx, i * 10, i * 10, 10);
-    }
-  });
+  changeBackground(BG_GREEN, ()=>drawPlayers());
 }
-
+function drawPlayers(){
+  for (let i = 1; i <= 12; i++) {
+    playerComponent(i * 10, i * 10, 10);
+  }
+}
 //@to is a string that reperesent the image path bc why not;
-function changeBackground(ctx, imagePath, callback) {
+function changeBackground(imagePath, callback) {
   const bgImage = new Image(); // Create a new image element
   bgImage.src = imagePath;
   bgImage.onload = () => {
     // Draw the background when the image has loaded
     ctx.drawImage(bgImage, 0, 0, ctx.canvas.width, ctx.canvas.height);
     if (callback) callback();
-
   };
 
 }
 //create that function if you want to create another player;
-function playerComponent(ctx, x, y, radius) {
+function playerComponent(x, y, radius) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2 * Math.PI); // Draw a circle
   ctx.fillStyle = "blue"; // Set player color
--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,24 @@
+const WHITE_BG_URL = '../Images/Background_White.png';
+const GREEN_BG_URL = '../Images/Background_Green.png';
+
+
+
 const Elements = [
   {
     name: "Backgrounds",
     panelItems: 
     [
       {
-        name: "Red",
-        functionalty: ()=>{console.log("This is Red");}
-      },
-      {
         name: "Green",
-        functionalty: ()=>{console.log("This is Green");}
+        functionalty: ()=>{changeBackground(GREEN_BG_URL, ()=>{
+          drawPlayers();
+        })}
       },
       {
-        name: "Blue",
-        functionalty: ()=>{console.log("This is Blue");}
+        name: "White",
+        functionalty: ()=>{changeBackground(WHITE_BG_URL, ()=>{
+          drawPlayers();
+        })}
       }
     ]
   },
@@ -72,27 +77,39 @@ function CreateNavigationBar(){
   const parentNavDiv = document.querySelector(".navigation-bar");
   
   Elements.forEach((element)=>{
+
+    
     const parentDivOfButtonAndPanel = document.createElement("div");
     parentDivOfButtonAndPanel.classList.add("nav-button-parent-div");
     parentNavDiv.appendChild(parentDivOfButtonAndPanel);
 
+    //Navigation Element
     const newButton = document.createElement("button");
     
     newButton.classList.add("nav-bar-button");
     
     newButton.innerHTML = `${element.name}`;
     parentDivOfButtonAndPanel.appendChild(newButton);
+
+    //we are initilaizng the inner panel for every navigation button.
     createLists(parentDivOfButtonAndPanel, element, parentDivOfButtonAndPanel);
   });
 }
 //element is the array that we take from buttons object props.
 function createLists(parent, element, button){
+
+  //we are creating the Panel UL
   let newUl = document.createElement("ul");
+  //for styling with css
   newUl.classList.add("element-of-nav-bar-panel");
+
+  //It's gonna take all items for every declared element (i.e panel items object like navigation button etc.)
   for(let i = 0; i < element.panelItems.length; i++){
+    //creating the items in the panel
     let newLi = document.createElement("li");
     newLi.id = `${element.panelItems[i].name}-panel-item-${i}`;
     newLi.innerHTML = `${element.panelItems[i].name}`;
+    newLi.onmousedown = ()=>{element.panelItems[i].functionalty();}
     newUl.appendChild(newLi);
   }
   button.onmouseenter = ()=>navBarEnter(newUl);
