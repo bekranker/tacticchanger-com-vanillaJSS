4625177bd9ba97f9aded2d5ffe9c011db635abb7
Bekir <aqwwwe73@gmail.com>
2024-12-03 03:32:57 +0300

    I accidently delete all branch. I just wrote my last 3 hours again. I feel pain, I feel anger, I never search git command anymore.

--- JS/Draw.js
+++ JS/Draw.js
@@ -1,33 +1,34 @@
 let marks = [];
 let CanDraw = false;
 let CanErase = false;
+let StartPosition = {x: 0, y: 0};
+
 const offset = 50;
+
 class DrawPenc{
   constructor(positions,  radius, colors, name){
     this.position = positions;
     this.radius = radius;
     this.colors = colors;
     this.name = name;
-    console.log(`X:${this.position.x}\n`);
-    console.log(`Y:${this.position.y}\n`);
-    registerRenderer(this.render);
   }
-  render = ()=>{
-    this.clearMe();
-    this.drawMe();
-    
+  render = (e, rect)=>{
+    this.drawMe(e, rect);
   }
-  drawMe = () =>{
+  drawMe = (e, rect) =>{
+    console.log("drawing");
     ctx.beginPath();
-    ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
-    ctx.fillStyle = this.colors.backgroundColor; // Set player color
-    ctx.fill(); // Fill the circle
-    ctx.strokeStyle = this.colors.borderColor; // Optional: Outline the player
-    ctx.lineWidth = 3;
+    const tempMousePosition = MousePos(e, rect);
+    ctx.moveTo(StartPosition.x, StartPosition.y);
+    ctx.lineTo(tempMousePosition.x, tempMousePosition.y);
+    StartPosition = MousePos(e, rect);
+    ctx.lineStyle = "black";
+    ctx.lineWidth = 2;
     ctx.stroke(); // Stroke the circle
   }
-  clearMe = ()=>{
-    ctx.clearRect(this.position.x - 10, this.position.y - 10, 100, 100);
+  clearMe = (e, rect)=>{
+    const tempPos = MousePos(e, rect);
+    ctx.clearRect(tempPos.x, tempPos.y, 20, 20);
   }
   DeleteMe = () =>{
     this.clearMe();
@@ -36,31 +37,41 @@ class DrawPenc{
 }
 
 
-function mousePos(e, rect){
+function MousePos(e, rect){
   const tempPos = {x: 0, y: 0}
-  tempPos.x = e.clientX - rect.left - 10;
-  tempPos.y = e.clientY - rect.top - 130;
+  tempPos.x = e.clientX - rect.left;
+  tempPos.y = e.clientY - rect.top;
   return tempPos;
 }
-function DrawPencils(e, canvas){
-  if(!CanDraw) return;
-  const tempMark = new DrawPenc(mousePos(e, canvas), 1, {backgroundColor: "#000000", borderColor: "#000000"}, `mark`);
+function DrawPencils(e, rect){
+  if (!CanDraw) return;
+  const tempMark = new DrawPenc(MousePos(e, rect), 1, {backgroundColor: "#000000", borderColor: "#ffffff"}, `mark`);
+  tempMark.render(e, rect);
   if (!marks.includes(tempMark)) {
     marks.push(tempMark);
   }
 }
 function erase(e, rect){
   if (!CanErase) return;
-  const tempMousePos = mousePos(e, rect);
+  const tempMousePos = MousePos(e, rect);
   marks.forEach((item, index)=>{
     if ((tempMousePos.x >= item.position.x -offset &&
       tempMousePos.x <= item.position.x +offset &&
       tempMousePos.y >= item.position.y -offset &&
       tempMousePos.y <= item.position.y +offset)) 
       {
-        item.DeleteMe();
-        marks.splice(index, 1);
+        item.clearMe(e, rect);
       }
   });
+}
 
+let startDrawing = false;
+
+
+function StartDrawing(e, rect){
+  StartPosition = MousePos(e, rect);
+  startDrawing = true;
+}
+function StopDrawing(){
+  startDrawing = false;
 }
--- JS/Play Field/SoccerField.js
+++ JS/Play Field/SoccerField.js
@@ -1,26 +1,32 @@
 const BG_GREEN = '../../Images/Background_Green.png';
-
 let ctx;
+let rect;
 // This is the main call for all render and create seccions;
 function DrawCanvas(){
   const canvas = document.createElement("canvas");
-  const rect = canvas.getBoundingClientRect();
   ctx = canvas.getContext('2d');
-
-  const canvasParent = document.getElementById("canvas-parent");
-
   canvas.width = 1200;
   canvas.height = 800;
   canvas.classList.add("canvas");
+  
+  const canvasParent = document.getElementById("canvas-parent");
   canvasParent.appendChild(canvas);
-  drawPlayers();
-  canvas.onclick = (e)=>{DrawPencils(e, rect);
-    erase(e, rect);};
+  rect = canvas.getBoundingClientRect();
 
+  
+  
+  drawPlayers();
+  canvas.onmousedown = (e) => {StartDrawing(e, rect);}
+  canvas.onmouseup = (e) => {StopDrawing(e, rect);}
+  canvas.onmousemove = (e)=>{
+    if (!startDrawing) return;
+    DrawPencils(e, rect);
+    erase(e, rect);
+  };
   
 }
 function drawPlayers(){
-  for (let i = 1; i <= 1; i++) {
+  for (let i = 1; i <= 12; i++) {
     const newPlayer = new Player({x: i * 100, y: 10}, 10, {backgroundColor: "#000000", borderColor: "#ffffff"}, `Player ID${i}`);
   }
 }
